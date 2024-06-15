4d61a5e4536035637d5e989278edf3d03c937bbe
nabind47 <nabindhami619@gmail.com>
2024-06-15 08:21:57 +0545

    feat: bookings

--- src/pages/hostels/Hostel.jsx
+++ src/pages/hostels/Hostel.jsx
@@ -1,5 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Hostel = () => {
-  return <div>Hostel</div>;
+  const params = useParams();
+  const [hostel, setHostel] = useState({});
+
+  const fetchHostel = async () => {
+    const resposne = await axios.get(
+      `http://localhost:8000/api/hostels/${params.hostel}`
+    );
+    setHostel(resposne.data);
+  };
+  useEffect(() => {
+    fetchHostel();
+  }, []);
+
+  console.log(hostel);
+
+  return (
+    <div className="grid sm:grid-cols-4 gap-4">
+      <div>
+        {/* <img src={hostel.image} alt="" /> */}
+        <img
+          src={
+            "https://t4.ftcdn.net/jpg/02/19/66/93/360_F_219669327_v12pBKc7TB62E3uCJrgRRkDhfVENK3z5.jpg"
+          }
+          className="rounded-md"
+          alt=""
+        />
+        <h2>{hostel.name}</h2>
+        <p>{hostel.location}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Hostel;
--- src/pages/hostels/Hostels.jsx
+++ src/pages/hostels/Hostels.jsx
@@ -1,5 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Hostels = () => {
-  return <div>Hostels</div>;
+  const [hostels, setHostels] = useState([]);
+
+  const fetchHostels = async () => {
+    const resposne = await axios.get(`http://localhost:8000/api/hostels`);
+    setHostels(resposne.data);
+  };
+  useEffect(() => {
+    fetchHostels();
+  }, []);
+
+  return (
+    <div className="grid sm:grid-cols-4 gap-4">
+      {hostels.map((hostel) => (
+        <div key={hostel.id}>
+          <Link to={`/hostels/${hostel.id}`}>
+            <img
+              src={
+                "https://t4.ftcdn.net/jpg/02/19/66/93/360_F_219669327_v12pBKc7TB62E3uCJrgRRkDhfVENK3z5.jpg"
+              }
+              className="rounded-md"
+              alt=""
+            />
+            {/* <img src={hostel.image} alt="" /> */}
+
+            <h2>{hostel.name}</h2>
+            <p>{hostel.location}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Hostels;
